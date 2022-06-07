19a25add2e044693b2f0f16f0e0081411bada256
kevinstadler <kevinstadler@users.noreply.github.com>
2022-06-07 15:36:45 +0200

    Implement stochastic pixel changes and fixed on-time length

--- interestingneomatrix/pixel.h
+++ interestingneomatrix/pixel.h
@@ -1,40 +1,65 @@
-#define MIN_HUE_DISTANCE 3640 // ~20 degrees
-uint16_t pseudoRandomHue(uint16_t lastHue) {
-  uint16_t n = lastHue + MIN_HUE_DISTANCE + random(0, 65535 - 2*MIN_HUE_DISTANCE);
-  Serial.printf("%u > %u\n", lastHue, n);
-  return n;
-}
-
 class Pixel {
   public:
+    Pixel() {
+      this->hue = transitionP;
+    };
     virtual ~Pixel() {};
-    virtual uint32_t getColor(uint32_t t);
+    CRGB getColor(uint32_t t) {
+      // gamma correction is last: http://renderwonk.com/blog/index.php/archive/adventures-with-gamma-correct-rendering/
+//      return CONFIG.CORRECT_GAMMA ? matrix.gamma32(getRawColor(t)) : getRawColor(t);
+      return getRawColor(t);
+    }
     uint16_t index;
   protected:
+    #ifdef FASTLED
+    byte hue;
+    #else
     uint16_t hue;
+    #endif
+    virtual CRGB getRawColor(uint32_t t);
+    uint16_t effectiveHue(uint16_t hue) {
+      #ifdef FASTLED
+      return CONFIG.HUE_OFFSET + hue << (8 - CONFIG.HUE_BITS);
+      #else
+      return CONFIG.HUE_OFFSET + hue << (16 - CONFIG.HUE_BITS);
+      #endif
+    }
+    CRGB effectiveColor(uint16_t hue) {
+      // https://learn.adafruit.com/adafruit-neopixel-uberguide/arduino-library-use
+      return CHSV(this->effectiveHue(hue), CONFIG.SATURATION, CONFIG.VALUE);
+    }
+    boolean shouldChange(uint32_t p = transitionP) {
+      return (p == PROBABILITY_BASE) || (random(PROBABILITY_BASE) <= p);
+    }
+    uint32_t getNewStartTime(uint32_t t) {
+      return t + CONFIG.TRANSITION_DURATION;
+    }
+    uint32_t getNewEndTime(uint32_t t) {
+      return t + CONFIG.TRANSITION_DURATION +
+        (CONFIG.RANDOM_INTERVAL ? random(min(CONFIG.ON_MIN, CONFIG.ON_MAX), max(CONFIG.ON_MIN, CONFIG.ON_MAX)) : CONFIG.ON_MIN);
+    }
 };
-
-enum PixelType { Synced, Unsynced, Shifting, Adding };
 
 class SyncedPixel : public Pixel {
   protected:
     static uint32_t timeout;
     static uint16_t nSynced;
   public:
-    uint32_t getColor(uint32_t t) {
-      if (t >= this->timeout + OFF_TIME) {
-        this->timeout = t + (RANDOM_INTERVAL ? random(ON_MIN, ON_MAX) : ON_MIN);
+    SyncedPixel() {
+      this->timeout = millis();
+    }
+    CRGB getRawColor(uint32_t t) {
+      if (t >= this->timeout) {// + CONFIG.OFF_TIME) {
+        this->timeout = this->getNewEndTime(t);
         this->nSynced = 0;
       } else if (t >= this->timeout) {
         return 0;
       }
-      if (nSynced != nLit) {
+      if (this->nSynced != nLit) {
         this->hue = random();
         this->nSynced++;
       }
-      // https://learn.adafruit.com/adafruit-neopixel-uberguide/arduino-library-use
-      uint32_t col = matrix.ColorHSV(HUE_OFFSET + this->hue << (16 - HUE_BITS), SATURATION, VALUE);
-      return CORRECT_GAMMA ? matrix.gamma32(col) : col;
+      return this->effectiveColor(this->hue);
     }
 };
 
@@ -48,65 +73,74 @@ class UnsyncedPixel : public Pixel {
     UnsyncedPixel() {
       this->timeout = millis();
     }
-    uint32_t getColor(uint32_t t) {
-      if (t >= this->timeout + OFF_TIME) {
+    CRGB getRawColor(uint32_t t) {
+      if (t >= this->timeout) {// + CONFIG.OFF_TIME) {
         this->hue = random();
-        this->timeout = t + (RANDOM_INTERVAL ? random(ON_MIN, ON_MAX) : ON_MIN);
+        this->timeout = this->getNewEndTime(t);
       }
 //      if (t >= this->timeout) {
 //        return 0;
 //      } else {
         // https://learn.adafruit.com/adafruit-neopixel-uberguide/arduino-library-use
-        uint32_t col = matrix.ColorHSV(HUE_OFFSET + this->hue << (16 - HUE_BITS), SATURATION, VALUE);
-        return CORRECT_GAMMA ? matrix.gamma32(col) : col;
+        return this->effectiveColor(this->hue);
 //      }
     }
 };
 
-class BlendingPixel : public UnsyncedPixel {
+class BlendingPixel : public Pixel {
   protected:
     uint32_t startTime;
+    #ifdef FASTLED
+    byte nextHue;
+    #else
     uint16_t nextHue;
-    uint32_t nextTime;
-    virtual uint32_t getBlendedColor(byte progress);
+    #endif
+    uint32_t endTime;
+    virtual CRGB getBlendedColor(byte progress);
   public:
-    uint32_t getColor(uint32_t t) {
-    if (t >= this->nextTime) {
-      this->startTime = t;
-      this->nextTime = t + (RANDOM_INTERVAL ? random(ON_MIN, ON_MAX) : ON_MIN);
-      this->hue = this->nextHue;
-      this->nextHue = random();
-    }
-    // scale transition time to [0,255]
-    byte progress = (255 * (t - this->startTime)) / (this->nextTime - this->startTime);
-    // TODO use sine transform
-    return this->getBlendedColor(progress);
-  }
-};
-
-class ShiftingPixel : public BlendingPixel {
-  protected:
-    uint32_t getBlendedColor(byte progress) {
-      // linearly interpolate hue
-      uint32_t col = matrix.ColorHSV(HUE_OFFSET + ((255-progress)*this->hue + progress*this->nextHue) / 255, SATURATION, VALUE);
-      return CORRECT_GAMMA ? matrix.gamma32(col) : col;
-    }
-};
-
-class AddingPixel : public BlendingPixel {
-  protected:
-    uint32_t getBlendedColor(byte progress) {
-      // weighted average of the *colors*
-      uint32_t col1 = matrix.ColorHSV(HUE_OFFSET + this->hue, SATURATION, VALUE);
-      uint32_t col2 = matrix.ColorHSV(HUE_OFFSET + this->nextHue, SATURATION, VALUE);
-      // TODO bitwise each
-      uint32_t col;
-      for (byte i = 0; i < 3; i++) {
-        col |= ((((255-progress)*((col1 >> 8*i) & 0xFF) + progress*((col2 >> 8*i) & 0xFF))) / 255) << 8*i;
+    CRGB getRawColor(uint32_t t) {
+      if (t >= this->endTime && this->shouldChange()) {
+        this->startTime = this->getNewStartTime(t);
+        this->endTime = this->getNewEndTime(t);
+        this->hue = this->nextHue;
+        this->nextHue = random();
       }
-      return col;
+      // scale transition time to [0,255] -- TODO use sine transform instead of linear
+      byte progress = (t < this->startTime) ? 255 - (255 * (this->startTime - t)) / CONFIG.TRANSITION_DURATION : 255;
+      return this->getBlendedColor(progress);
     }
 };
+
+// pure hues
+class RadialBlendingPixel : public BlendingPixel {
+  protected:
+    CRGB getBlendedColor(byte progress) {
+      // linearly interpolate hue
+      // FIXME this breaks with bytes because of overflow
+      return this->effectiveColor(((255-progress)*this->hue + progress*this->nextHue) / 255);
+    }
+};
+
+// peachy
+class LinearBlendingPixel : public BlendingPixel {
+  protected:
+    CRGB getBlendedColor(byte progress) {
+      // weighted average of the *colors*
+      CRGB col1 = this->effectiveColor(this->hue);
+      CRGB col2 = this->effectiveColor(this->nextHue);
+      // TODO bitwise each
+//      uint32_t col;
+      for (byte i = 0; i < 3; i++) {
+        col1[i] = ((255-progress)*col1[i] + progress*col2[i]) / 255;
+//        col |= ((((255-progress)*((col1 >> 8*i) & 0xFF) + progress*((col2 >> 8*i) & 0xFF))) / 255) << 8*i;
+      }
+      return col1;
+    }
+};
+
+// TODO implement permanently shifting type
+
+// steadystate can be: fixed or wandering (permanent hue-walk, in either direction?)
 
 /*
       if (FADE_IN) {
